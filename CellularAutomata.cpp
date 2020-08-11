c91e9ded34018e6dd1ce727d25c920676342665b
manifactory <mobrother03@gmail.com>
2020-08-11 13:35:49 +0900

    예외 클래스 반환

---
+++ CellularAutomata.cpp
@@ -0,0 +1,21 @@
+#include "CellularAutomata.h"
+
+CellularAutomata::CellularAutomata()
+{
+}
+
+CellularAutomata::~CellularAutomata()
+{
+}
+
+void CellularAutomata::init()
+{
+}
+
+void CellularAutomata::update()
+{
+}
+
+void CellularAutomata::draw()
+{
+}
--- CellularAutomata.h
+++ CellularAutomata.h
@@ -1,4 +1,18 @@
 #pragma once
+#include "SDL.h"
+
+typedef struct vector_2d {
+	float x;
+	float y;
+}vector_2d;
+
+typedef struct particle_t {
+	int id;
+	float life_time;
+	vector_2d velocity;
+	SDL_Color color;
+	bool has_been_updated;
+}particle_t;
 
 class CellularAutomata
 {
