17fbdcea943fa06966ee2defeb7de6a7bb5fb36e
mobrother03 <mobrother03@sunrint.hs.kr>
2020-08-16 09:05:10 +0900

    Texture Manager

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -1,1 +1,15 @@
-#include "TextureManager.h"
+#include "textureManager.h"
+
+SDL_Texture* textureManager::loadTexture(const char* texture)
+{
+	SDL_Surface* tempSurface = IMG_Load(texture);
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(Game::renderer, tempSurface);
+	SDL_FreeSurface(tempSurface);
+
+	return tex;
+}
+
+void textureManager::Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest)
+{
+	SDL_RenderCopy(Game::renderer, tex, &src, &dest);
+}
--- TextureManager.h
+++ TextureManager.h
@@ -1,6 +1,9 @@
 #pragma once
-class TextureManager
-{
 
+#include  "Game.h"
+
+class textureManager {
+public:
+	static SDL_Texture* loadTexture(const char* filename);
+	static void Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest);
 };
-
